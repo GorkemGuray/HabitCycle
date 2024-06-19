de17a77fe156e381d971a673059905fa37476700
Görkem Güray <grkmgry@gmail.com>
2024-06-19 21:33:37 +0300

    Initial Commit

---
+++ HabitCycle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HabitCycle
+//
+//  Created by Görkem Güray on 19.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HabitCycle/HabitCycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitCycleApp.swift
+//  HabitCycle
+//
+//  Created by Görkem Güray on 19.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitCycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
